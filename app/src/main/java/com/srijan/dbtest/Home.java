a9a7e3162e453add34a6daa8ab7c7ca692cc5994
srijansood <srijansood@gmail.com>
2015-02-13 13:58:35 -0500

    M4

---
+++ app/src/main/java/com/srijan/dbtest/Home.java
@@ -0,0 +1,50 @@
+package com.srijan.dbtest;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class Home extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Starts LoginActivity
+     * @param view
+     */
+    public void callLogin(View view) {
+        Intent i = new Intent(getBaseContext(), LoginActivity.class);
+        startActivityForResult(i, 1);
+    }
+}
